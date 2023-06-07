941cb6cd7d22e410ca09b5cea205175ccb5f182b
jmonto55 <jmonto55@gmail.com>
2023-06-07 00:26:22 -0500

    Connect React app with local host Rails api

---
+++ src/greetingSlice.js
@@ -0,0 +1,37 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchGreeting = createAsyncThunk(
+  'greeting/fetchGreeting',
+  async () => {
+    const response = await fetch('http://localhost:3000/messages');
+    const data = await response.json();
+    return data.message;
+  },
+);
+
+const greetingSlice = createSlice({
+  name: 'greeting',
+  initialState: {
+    value: '',
+    status: 'idle',
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchGreeting.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchGreeting.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        console.log(action.payload);
+        state.value = action.payload;
+      })
+      .addCase(fetchGreeting.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default greetingSlice.reducer;
